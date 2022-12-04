3c0557d5505513374f57608a23149f6679f401d2
Jordan Lewis <jordanthelewis@gmail.com>
2022-12-03 21:18:57 -0500

    2022 day 2 part 2

--- 2022/day2/src/main.zig
+++ 2022/day2/src/main.zig
@@ -25,6 +25,14 @@ pub fn parseRps(in: u8) !rps {
 // 0 if loss, 3 if draw, 6 if won
 var rpsScores: [3][3]u8 = .{ .{ 3, 0, 6 }, .{ 6, 3, 0 }, .{ 0, 6, 3 } };
 
+// map that gives you the rps you need to play to lose/draw/win the first rps.
+// rps -> lose/draw/win -> rps
+var rpsKey: [3][3]rps = .{
+    .{ rps.scissors, rps.rock, rps.paper },
+    .{ rps.rock, rps.paper, rps.scissors },
+    .{ rps.paper, rps.scissors, rps.rock },
+};
+
 pub fn evalRps(l: rps, r: rps) u8 {
     const i = @enumToInt(l);
     const j = @enumToInt(r);
@@ -36,6 +44,7 @@ pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
     var buffer: [1024]u8 = undefined;
     var scoreSum: i64 = 0;
+    var part2ScoreSum: i64 = 0;
     while (try stdin.readUntilDelimiterOrEof(buffer[0..], '\n')) |line| {
         const them = try parseRps(line[0]);
         const us = try parseRps(line[2]);
@@ -44,6 +53,12 @@ pub fn main() !void {
         // Add 1 since the enum is 0-indexed.
         const total = @enumToInt(us) + 1 + score;
         scoreSum += total;
+
+        // Part 2: X means you lose, Y means you draw, Z means you win.
+        const shouldPlay = rpsKey[@enumToInt(them)][@enumToInt(us)];
+        const part2total = @enumToInt(shouldPlay) + 1 + (@enumToInt(us) * 3);
+        part2ScoreSum += part2total;
     }
-    try stdout.print("{d}\n", .{scoreSum});
+    try stdout.print("part 1: {d}\n", .{scoreSum});
+    try stdout.print("part 2: {d}\n", .{part2ScoreSum});
 }
