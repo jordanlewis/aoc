d0d0df7a4e7a29da502ec71f75c3256fe76e3c49
Jordan Lewis <jordanthelewis@gmail.com>
2022-12-04 00:14:09 -0500

    2022 day4

--- 2022/day4/src/main.zig
+++ 2022/day4/src/main.zig
@@ -8,8 +8,23 @@ pub fn main() !void {
     var part2: u64 = 0;
     var nLine: u64 = 0;
     while (try stdin.readUntilDelimiterOrEof(buffer[0..], '\n')) |line| {
-        for (line) |val, i| {
-            try stdout.print("{d}:{d} = {d}\n", .{ nLine, i, val });
+        var iter = std.mem.split(u8, line, ",");
+        var l = iter.next().?;
+        var liter = std.mem.split(u8, l, "-");
+        var r = iter.next().?;
+        var riter = std.mem.split(u8, r, "-");
+        const a = (try std.fmt.parseInt(i32, liter.next().?, 10));
+        const b = (try std.fmt.parseInt(i32, liter.next().?, 10));
+        const c = (try std.fmt.parseInt(i32, riter.next().?, 10));
+        const d = (try std.fmt.parseInt(i32, riter.next().?, 10));
+        try stdout.print("{d} {d} {d} {d}\n", .{ a, b, c, d });
+
+        if ((a <= c and b >= d) or (c <= a and d >= b)) {
+            part1 += 1;
+        }
+
+        if ((a >= c and a <= d) or (b >= c and b <= d) or (c >= a and c <= b) or (d >= a and d <= b)) {
+            part2 += 1;
         }
         nLine += 1;
     }
