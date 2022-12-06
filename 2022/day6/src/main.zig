5a9d26815ce8474ef021f696896cfd730a313198
Jordan Lewis <jordanthelewis@gmail.com>
2022-12-06 00:39:03 -0500

    "golf" day 6

--- 2022/day6/src/main.zig
+++ 2022/day6/src/main.zig
@@ -1,45 +1,18 @@
 const std = @import("std");
-const ArrayList = std.ArrayList;
-const tokenize = std.mem.tokenize;
 
 pub fn main() !void {
     const stdin = std.io.getStdIn().reader();
-    const stdout = std.io.getStdOut().writer();
-    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    defer arena.deinit();
-    const allocator = arena.allocator();
-    const ptr = try allocator.create(i32);
-    std.debug.print("ptr={*}\n", .{ptr});
-
     var buffer: [4096]u8 = undefined;
-    var part1: u64 = 0;
-    var part2: u64 = 0;
-    var nLine: u64 = 0;
-    while (try stdin.readUntilDelimiterOrEof(buffer[0..], '\n')) |line| {
-        for (line) |_, i| {
-            if (i < 14) {
-                continue;
-            }
-            var j: u8 = 0;
-            var chars: [26]u8 = std.mem.zeroes([26]u8);
-            while (j < 14) : (j += 1) {
-                try stdout.print("{s}\n", .{[_]u8{line[i - j]}});
-                chars[line[i - j] - 'a'] += 1;
-            }
-            var found = true;
-            for (chars) |char| {
-                if (char > 1) {
-                    found = false;
-                    break;
-                }
-            }
-            if (found) {
-                try stdout.print("part 1: {}\n", .{i + 1});
-                break;
-            }
+    _ = try stdin.readAll(buffer[0..]);
+    outer: for (buffer[14..]) |_, i| {
+        var j: u8 = 0;
+        var chars: [26]u2 = std.mem.zeroes([26]u2);
+        while (j < 14) : (j += 1) {
+            var x: *u2 = &chars[buffer[i + 14 - j] - 'a'];
+            x.* += 1;
+            if (x.* > 1) continue :outer;
         }
-        nLine += 1;
+        std.debug.print("{}\n", .{i + 15});
+        break;
     }
-    try stdout.print("part 1: {d}\n", .{part1});
-    try stdout.print("part 2: {d}\n", .{part2});
 }
