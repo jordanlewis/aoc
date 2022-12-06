b75919d32eacdb8f61107fd89a997e41cb96a30c
Jordan Lewis <jordanthelewis@gmail.com>
2022-12-05 23:52:24 -0500

    update tempalte

--- 2022/zigtemplate/src/main.zig
+++ 2022/zigtemplate/src/main.zig
@@ -1,8 +1,16 @@
 const std = @import("std");
+const ArrayList = std.ArrayList;
+const tokenize = std.mem.tokenize;
 
 pub fn main() !void {
     const stdin = std.io.getStdIn().reader();
     const stdout = std.io.getStdOut().writer();
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
+    const ptr = try allocator.create(i32);
+    std.debug.print("ptr={*}\n", .{ptr});
+
     var buffer: [1024]u8 = undefined;
     var part1: u64 = 0;
     var part2: u64 = 0;
