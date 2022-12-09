7e5e53a0196daa9d305209f479f2da0b59a816a2
Jordan Lewis <jordanthelewis@gmail.com>
2022-12-09 01:23:27 -0500

    dfijijsldkfj

--- 2022/day9/src/main.zig
+++ 2022/day9/src/main.zig
@@ -14,62 +14,43 @@ pub fn main() !void {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
     const allocator = arena.allocator();
-    const ptr = try allocator.create(i32);
-    print("ptr={*}\n", .{ptr});
 
     var buffer: [1024]u8 = undefined;
     var part1: u64 = 0;
-    var part2: u64 = 0;
-    var nLine: u64 = 0;
     var grid = std.AutoHashMap(point, bool).init(allocator);
     const nTails = 10;
     var snake: [nTails]point = std.mem.zeroes([nTails]point);
     try grid.put(snake[nTails - 1], true);
-    while (try stdin.readUntilDelimiterOrEof(buffer[0..], '\n')) |line| : (nLine += 1) {
-        var iter = tokenize(u8, line, " ");
-        var dir = iter.next().?;
-        var n = try parseInt(i8, iter.next().?, 10);
+    while (try stdin.readUntilDelimiterOrEof(buffer[0..], '\n')) |line| {
+        var dir = line[0];
+        var n = try parseInt(i8, line[2..], 10);
         var i: usize = 0;
         while (i < n) : (i += 1) {
             var h = &snake[0];
-            switch (dir[0]) {
+            switch (dir) {
                 'R' => h.x += 1,
                 'L' => h.x -= 1,
                 'U' => h.y -= 1,
                 'D' => h.y += 1,
                 else => unreachable,
             }
-            print("head {s}\n", .{dir});
             var j: usize = 1;
             while (j < snake.len) : (j += 1) {
                 h = &snake[j - 1];
                 var t = &snake[j];
                 var xdiff = (h.x - t.x);
                 var ydiff = (h.y - t.y);
-                if (xdiff >= -1 and xdiff <= 1 and ydiff >= -1 and ydiff <= 1) {
-                    continue;
-                }
-                if (t.y < h.y) {
-                    t.y += 1;
-                } else if (t.y > h.y) {
-                    t.y -= 1;
-                }
-                if (t.x < h.x) {
-                    t.x += 1;
-                } else if (t.x > h.x) {
-                    t.x -= 1;
-                }
-                print("moving {} -> {},{}\n", .{ j, t.x, t.y });
+                if (try std.math.absInt(xdiff) < 2 and try std.math.absInt(ydiff) < 2) continue;
+                if (t.y != h.y) t.y += std.math.sign(ydiff);
+                if (t.x != h.x) t.x += std.math.sign(xdiff);
             }
             try grid.put(snake[nTails - 1], true);
         }
-        print("{s}, {}\n", .{ dir, n });
     }
     var iterator = grid.iterator();
     while (iterator.next()) |_| {
         part1 += 1;
     }
 
-    print("part 1: {d}\n", .{part1});
-    print("part 2: {d}\n", .{part2});
+    print("{d}\n", .{part1});
 }
