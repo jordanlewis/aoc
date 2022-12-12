3c371063c85714a9c29a6818342a112fb140f05d
Jordan Lewis <jordanthelewis@gmail.com>
2022-12-12 01:11:56 -0500

    format

--- 2022/day12/src/main.zig
+++ 2022/day12/src/main.zig
@@ -20,8 +20,8 @@ pub fn main() !void {
     var nLine: u64 = 0;
     var grid: [41][179]u8 = std.mem.zeroes([41][179]u8);
     var distances: [41][179]u16 = std.mem.zeroes([41][179]u16);
-    var start= point { .x = 0, .y = 0};
-    var end = point{.x=0, .y=0};
+    var start = point{ .x = 0, .y = 0 };
+    var end = point{ .x = 0, .y = 0 };
     var nCol: u64 = 0;
     while (try stdin.readUntilDelimiterOrEof(buffer[0..], '\n')) |line| : (nLine += 1) {
         nCol = line.len;
@@ -40,9 +40,9 @@ pub fn main() !void {
     }
 
     var i: usize = 0;
-    while (i < nLine) : (i+=1) {
+    while (i < nLine) : (i += 1) {
         var j: usize = 0;
-        while (j < nCol) : (j +=1) {
+        while (j < nCol) : (j += 1) {
             if (grid[i][j] != 'a' and grid[i][j] != 'S') continue;
             start.x = @intCast(i64, j);
             start.y = @intCast(i64, i);
@@ -52,9 +52,9 @@ pub fn main() !void {
             while (q.items.len > 0) {
                 var p = q.orderedRemove(0);
                 var dist = distances[@intCast(usize, p.y)][@intCast(usize, p.x)];
-                for ([_]point{.{.x=-1, .y=0}, .{.x=1, .y=0}, .{.x=0,.y=-1}, .{.x=0,.y=1}}) |dir| {
-                    var newXI = p.x+dir.x;
-                    var newYI = p.y+dir.y;
+                for ([_]point{ .{ .x = -1, .y = 0 }, .{ .x = 1, .y = 0 }, .{ .x = 0, .y = -1 }, .{ .x = 0, .y = 1 } }) |dir| {
+                    var newXI = p.x + dir.x;
+                    var newYI = p.y + dir.y;
                     if (newXI < 0 or newXI >= nCol) continue;
                     if (newYI < 0 or newYI >= nLine) continue;
                     var newX = @intCast(usize, newXI);
@@ -64,7 +64,7 @@ pub fn main() !void {
                     if (oldDist == 0 or oldDist > dist + 1) {
                         distances[newY][newX] = dist + 1;
                         var newP = try allocator.create(point);
-                        newP.* = point{.x = newXI, .y = newYI};
+                        newP.* = point{ .x = newXI, .y = newYI };
                         try q.append(newP);
                     }
                 }
