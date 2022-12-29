101b8fe053b021589e1592c2ba3087fbf06c05e2
Jordan Lewis <jordanthelewis@gmail.com>
2022-12-28 20:23:38 -0500

    remove cruft from buildl

--- 2022/build.zig
+++ 2022/build.zig
@@ -37,13 +37,4 @@ pub fn build(b: *std.build.Builder) void {
         const run_step = b.step(dayString, run_desc);
         run_step.dependOn(&run_cmd.step);
     }
-
-    //const run_cmd = exe.run();
-    //run_cmd.step.dependOn(b.getInstallStep());
-    //if (b.args) |args| {
-    //    run_cmd.addArgs(args);
-    //}
-
-    //const run_step = b.step("run", "Run the app");
-    //run_step.dependOn(&run_cmd.step);
 }
